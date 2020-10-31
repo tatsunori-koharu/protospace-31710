8a2c8afce81dfae957a32629eea6b6afba3a91ce
tatsunori-koharu <tats208919ch@gmail.com>
2020-10-31 17:37:11 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,15 @@
+class Prototype < ApplicationRecord
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
+  belongs_to :user
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
+end
